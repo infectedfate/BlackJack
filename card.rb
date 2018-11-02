2e5af1c522a3b84c342ffe86c143fb971e53b94d
Malinin Mikhail <infectedfate@gmail.com>
2018-11-02 14:30:04 +0300

    continue

--- card.rb
+++ card.rb
@@ -1,2 +1,16 @@
 class Card
+
+  attr_reader :suit, :value
+
+  SUITS = ['+', '<>', '<3', '^'].freeze
+  VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'].freeze
+
+
+  def initialize(suit, value)
+    @suit = suit
+    @value = value
+  end
+
+  def validate_object
+  end
 end
--- player.rb
+++ player.rb
@@ -1,3 +1,8 @@
 class Player
   attr_accessor :name, :cash
+
+  def initialize(name)
+    @name = name
+    @cash = 100
+  end
 end
